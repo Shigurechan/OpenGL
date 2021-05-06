9395d4c12ca65d993a8a1b43d4c90d2fd33524d0
松澤賢 <yw3250118.2g2g@hotmail.com>
2021-05-06 20:26:13 +0900

    テキストレンダリングをを導入中

---
+++ OpenGL/Text.cpp
@@ -0,0 +1,27 @@
+#include "Text.hpp"
+#include <iostream>
+
+Text::Text()
+{
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+   //     return -1;
+    }
+
+    FT_Face face;
+    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    //    return -1;
+    }
+}
+
+
+
+
+Text::~Text()
+{
+
+}
---
+++ OpenGL/Text.hpp
@@ -0,0 +1,19 @@
+#ifndef ___TEXT_HPP
+#define ___TEXT_HPP
+
+#include "freetype/ft2build.h"
+#include FT_FREETYPE_H
+
+class Text
+{
+public:
+	Text();		//コンストラクタ
+	~Text();	//デストラクタ
+
+
+
+};
+
+
+
+#endif
